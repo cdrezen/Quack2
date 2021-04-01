4e8e907d5623ed7840767da3802bddcf41555ce5
remotequack <remotequack@hotmail.com>
2021-04-01 14:20:46 +0400

    caca

--- pygameTest/pygameTest ancien.py
+++ pygameTest/pygameTest ancien.py
@@ -19,6 +19,21 @@ class Acteur:
 def draw():
     screen.blit(background.animation[int(iFrames / (fps / background.framesCount))], background.position)
     quackFrame = quack.animation[int(iFrames / (fps / quack.framesCount))]
+    
+    
+    #rel_x, rel_y = mousePos[0] - quack.position[0], mousePos[1] - quack.position[1]
+
+    #if(abs(rel_x) > 4 or abs(rel_y) > 4):##sensibilité de 4px
+
+    #    quack.angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
+    #    print(quack.angle)###
+    #    quack.position = (mousePos[0], mousePos[1])
+
+
+    #quackFrame = pygame.transform.rotate(quackFrame, int(quack.angle - 90))     
+    #rect = quackFrame.get_rect(center=quack.position)  
+    
+    #print(rect)###
 
     screen.blit(quackFrame, quack.position)
 
@@ -26,7 +41,10 @@ def draw():
 
 
 pygame.init()
-pygame.display.set_caption("JEU BIEN")
+pygame.display.set_caption("QUACK 2.0")
+screen = pygame.display.set_mode(dimentions)
+logo = pygame.image.load("heart.png")
+pygame.display.set_icon(logo)
 
 background = Acteur([pygame.image.load("background0.png"), pygame.image.load("background1.png")], 2)
 quack = Acteur([pygame.image.load("quack0.png"), pygame.image.load("quack1.png")], 2, (dimentions[0] / 2, dimentions[1] / 2))
@@ -36,8 +54,7 @@ quack = Acteur([pygame.image.load("quack0.png"), pygame.image.load("quack1.png")
 while running:
 
     timer.tick(1000 / fps)
-
-    #gestion des evenements:
+    # event handling, gets all event from the event queue
     for event in pygame.event.get():
         if event.type == pygame.QUIT:# change the value to False, to exit the main loop
             running = False
@@ -50,8 +67,6 @@ while running:
 
 
 
-
-
-
+class 
 
 
